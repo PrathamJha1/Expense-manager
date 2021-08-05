a596e511d4a172d2d4c006e0ca22cdc9647d3df7
Pratham Jha <prathamjha5683@gmail.com>
2021-08-05 21:09:28 +0530

    deploying

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/Expense_Manager_db');
+mongoose.connect('mongodb+srv://admin-pratham:pratham123@cluster0.mhyxv.mongodb.net/Expense_Manager_db');
 const db = mongoose.connection;
 db.on('error', console.error.bind("error connectiog to db"));
 db.once('open', function () {
